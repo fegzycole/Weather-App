f03dfefb64484cf72ba6ab1330da734ce1e6126c
fegzycole <fergusoniyara@gmail.com>
2020-03-26 18:05:49 +0100

    Fix Stickler CI issues

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-unresolved
 import '@babel/polyfill';
 
 import dataController from './data';
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
             presets: ['@babel/preset-env'],
           },
         },
-      }
+      },
     ],
   },
 };
